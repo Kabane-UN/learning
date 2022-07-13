6c8df790f1ec4f0c2b21a30fe1e2c5757153103b
Kabane-UN <andryushka.kaban.z@gmail.com>
2022-07-13 11:02:58 +0300

    Десятый Комит

--- svg_editor/urls.py
+++ svg_editor/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import files_views
+from .views import files_views, files_save
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('files_views', files_views, name='files_views')
+    path('files_views', files_views, name='files_views'),
+    path('files_save', files_save, name='files_save')
 ]
--- svg_editor/views.py
+++ svg_editor/views.py
@@ -1,4 +1,5 @@
 import os.path
+from time import time
 
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -10,10 +11,19 @@ def index(request):
     return render(request, 'svg_editor/index.html')
 
 
+def files_save(request):
+    if request.method == "POST" and 'text' in dict(request.POST).keys():
+        path = os.path.join(BASE_DIR, 'svg_editor/media/svg_editor/svg')
+        name = int(time())
+        with open(path+'/'+str(name)+'.svg', 'w') as file:
+            file.write(dict(request.POST)['text'][0])
+    return JsonResponse({}, status=200)
+
+
 def files_views(request):
-    path = os.path.join(BASE_DIR, 'svg_editor/media/svg_editor/txt')
-    txt_list = os.listdir(path)
+    path = os.path.join(BASE_DIR, 'svg_editor/media/svg_editor/svg')
+    svg_list = os.listdir(path)
     response = {
-        'txts': txt_list
+        'svgs': svg_list
     }
     return JsonResponse(response)
