ad6c77a30608bba13f5ac86ad402ef006a80a6ae
Kabane-UN <andryushka.kaban.z@gmail.com>
2022-07-13 08:38:28 +0300

    Девятый Комит

--- svg_editor/urls.py
+++ svg_editor/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import files_views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('files_views', files_views, name='files_views')
 ]
--- svg_editor/views.py
+++ svg_editor/views.py
@@ -1,5 +1,19 @@
+import os.path
+
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from mysite.settings import BASE_DIR
 
 
 def index(request):
     return render(request, 'svg_editor/index.html')
+
+
+def files_views(request):
+    path = os.path.join(BASE_DIR, 'svg_editor/media/svg_editor/txt')
+    txt_list = os.listdir(path)
+    response = {
+        'txts': txt_list
+    }
+    return JsonResponse(response)
